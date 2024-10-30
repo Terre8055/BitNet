8788bb918261498ed7ca5528fc199da185ed2afa
Michael Appiah Dankwah <michaelappiah2018@icloud.com>
2024-10-30 09:55:07 +0000

    Update main file

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,12 +17,5 @@ python setup_env.py -md models/bitnet_b1_58-large -q i2_s
 python setup_env.py -md models/bitnet_b1_58-3B -q i2_s
 
 
-# Wait for the service to be ready
-echo "Waiting for the service to be ready..."
-until curl -s http://localhost:8000/health; do
-    echo "Service not ready, waiting..."
-    sleep 5  # Wait for 5 seconds before checking again
-done
-
 # Execute the main script
 exec python main.py
